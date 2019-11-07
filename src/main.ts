f8425fe9579e64e83f64af125c339da39279e944
tama <tama@ttk1.net>
2019-11-08 03:59:31 +0900

    一旦保存

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
+import * as THREE from 'three';
+
 window.onload = () => {
-  console.log('hoge');
+  main();
 };
+
+function main() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer();
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xddff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  const animate = () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    cube.rotation.x += 0.029;
+    cube.rotation.y += 0.031;
+    camera.updateProjectionMatrix();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  };
+
+  animate();
+}
