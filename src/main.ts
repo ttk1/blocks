ede3992abf1825f93bf4c74fc14b6b61f787fcf5
tama <tama@ttk1.net>
2019-11-09 01:00:58 +0900

    修正

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,32 @@ window.onload = () => {
 
 function main() {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 10);
 
   const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight, false);
   document.body.appendChild(renderer.domElement);
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xddff00 });
+  const material = new THREE.MeshLambertMaterial({ color: 0xddff00 });
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
-  camera.position.z = 5;
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(100, 200, 300).normalize();
+  scene.add(light);
+
+  camera.position.z = 2;
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize(window.innerWidth, window.innerHeight, false);
+    camera.updateProjectionMatrix();
+  });
 
   const animate = () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    cube.rotation.x += 0.029;
-    cube.rotation.y += 0.031;
-    camera.updateProjectionMatrix();
+    cube.rotation.x += 0.017;
+    cube.rotation.y += 0.023;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   };
