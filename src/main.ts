21a93757bf74790cfcb12013c65d8b5c5a321a12
tama <tama@ttk1.net>
2019-11-10 16:07:03 +0900

    https://github.com/ttk1/blocks/issues/1

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,43 @@ window.onload = () => {
 
 function main() {
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 10);
+  const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 150;
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight, false);
   document.body.appendChild(renderer.domElement);
 
-  addCube(0, 0, 0);
-  addCube(0, 1, 1);
-  addCube(1, 0, 1);
-  addCube(1, 1, 0);
+  // addCube(0, 0, 0);
+  // addCube(0, 1, 1);
+  // addCube(1, 0, 1);
+  // addCube(1, 1, 0);
 
-  function addCube(x: number, y: number, z: number) {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+  sphere();
+
+  function sphere() {
+    const geometry = new THREE.Geometry();
+    function addCube(x: number, y: number, z: number) {
+      const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
+      // const cube = new THREE.Mesh(new THREE.BoxGeometry(0.5, 0.5, 0.5));
+      cube.position.set(x, y, z);
+      geometry.mergeMesh(cube);
+    }
+
+    const r = 100;
+    for (let x = -r; x < r; x++) {
+      for (let y = -r; y < r; y++) {
+        for (let z = -r; z < r; z++) {
+          if (x ** 2 + y ** 2 + z ** 2 < r ** 2 &&
+            r ** 2 - 40 < x ** 2 + y ** 2 + z ** 2) {
+            addCube(x, y, z);
+          }
+        }
+      }
+    }
+
     const material = new THREE.MeshLambertMaterial({ color: 0xddff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(x, y, z);
-    scene.add(cube);
+    scene.add(new THREE.Mesh(geometry, material));
   }
 
   const light1 = new THREE.DirectionalLight(0xffffff);
@@ -33,8 +53,6 @@ function main() {
   const light2 = new THREE.DirectionalLight(0xffffff);
   light2.position.set(-300, -200, -100);
   scene.add(light2);
-
-  camera.position.z = 5;
 
   window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
